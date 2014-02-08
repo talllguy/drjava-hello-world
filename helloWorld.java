85e1b91badd755e6004dfc53e6f50a6b2f0f9b28
talllguy <elliott.plack@gmail.com>
2014-02-08 13:11:14 -0500

    got it working

---
+++ helloWorld.java
@@ -0,0 +1,11 @@
+// first program
+// elliott plack
+// 08 feb 2014
+
+public class helloWorld
+{
+  public static void main( String [] args )
+  {
+    System.out.println( "Is there anybody out there?" );
+  }
+}
